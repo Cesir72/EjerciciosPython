7e4c89d1555b62b2374bd77d4d0937a78fa44de1
Cesir72 <chechare71.gmail>
2024-05-01 17:53:54 +0200

    agragado nu

---
+++ listasAnidadas.py
@@ -0,0 +1,9 @@
+lista = [1, "a", True, [1, 2, ["f", "g","h"]]]
+
+print(lista)
+# br
+print(lista[0]) # retorna el numero 1
+
+print(lista[3] [1]) # retorna el numero 2
+
+print(lista [3] [2] [1]) # retorna la letra 'g'
